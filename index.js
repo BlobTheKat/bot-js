c67401594ff8db92881fa85589bb766921b4be2e
MatReiner <64211737+MatReiner@users.noreply.github.com>
2021-03-19 15:27:00 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-let util=require("util"),rl=require("readline").createInterface({input:process.stdin,output:process.stdout})
 module.exports=function(token,firetoken){
   let Discord = require("discord.js");
   Discord.Channel.prototype.svg = function(data){
@@ -8,6 +7,24 @@ module.exports=function(token,firetoken){
   let user = assetFor("users");
   let guild = assetFor("guilds");
   let bot = new Discord.Client();
+  bot.Discord = Discord;
+  Discord.Message.prototype.replyWith = Discord.Message.prototype.reply
+  Discord.Message.prototype.reply = async function reply(content, options) {
+    const mentionRepliedUser = typeof ((options || content || {}).allowedMentions || {}).repliedUser === "undefined" ? true : ((options || content).allowedMentions).repliedUser;
+    delete ((options || content || {}).allowedMentions || {}).repliedUser;
+    const apiMessage = content instanceof APIMessage ? content.resolveData() : APIMessage.create(this.channel, content, options).resolveData();
+    Object.assign(apiMessage.data, { message_reference: { message_id: this.id } });
+    if (!apiMessage.data.allowed_mentions || Object.keys(apiMessage.data.allowed_mentions).length === 0) apiMessage.data.allowed_mentions = { parse: ["users", "roles", "everyone"] };
+    if(typeof apiMessage.data.allowed_mentions.replied_user === "undefined") Object.assign(apiMessage.data.allowed_mentions, { replied_user: mentionRepliedUser });
+    if (Array.isArray(apiMessage.data.content)) {
+      return Promise.all(apiMessage.split().map(x => {
+        x.data.allowed_mentions = apiMessage.data.allowed_mentions;
+        return x;
+      }).map(this.inlineReply.bind(this)));
+    }
+    const { data, files } = await apiMessage.resolveFiles();
+    return this.client.api.channels[this.channel.id].messages.post({ data, files }).then(d => this.client.actions.MessageCreate.handle(d).message);
+  }
   bot.assets = {user, guild, save(){
     return Promise.all([
       user.save(),
@@ -140,8 +157,3 @@ module.exports=function(token,firetoken){
   });
   return bot;
 }
-console.log=(function(...a){process.stdout.write("\x0D\x1b[2K"+a.map(a=>typeof a=="string"?a:util.inspect(a,false,5,true)).join(" ")+"\n\x1b[34m> \x1b[m")}).bind(console)
-console.warn=(function(...a){process.stdout.write("\x0D\x1b[2K\x1b[33m"+a.map(a=>typeof a=="string"?a:util.inspect(a,false,5,true)).join(" ")+"\n\x1b[34m> \x1b[m")}).bind(console)
-console.error=(function(...a){process.stdout.write("\x0D\x1b[2K\x1b[31m"+a.map(a=>typeof a=="string"?a:util.inspect(a,false,5,true)).join(" ")+"\n\x1b[34m> \x1b[m")}).bind(console)
-console.in = console.input = console.ask = a => new Promise(r=>rl.question("\x1b[34m> \x1b[m",r));
-(async a=>{while(1){try{console.log(util.inspect(eval(await console.ask()),false,5,true))}catch(e){console.log("\x1b[31m"+e+"\x1b[m")}}})()
