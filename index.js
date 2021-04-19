2973b2150205435ec8a93be82911decdbfb8c605
MatReiner <64211737+MatReiner@users.noreply.github.com>
2021-04-19 11:02:51 +0100

    Support for contextual AI

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-module.exports=function(token,firetoken){
+module.exports=function(token="",firetoken="",options={ai:true,member:false,user:true,guild:true,clientOptions: {}, svgProxy: "https://svg.thei5pro.repl.co/svg.png?svg=$"}){
+  token+=""
+  options = Object.assign({ai:true,member:false,user:true,guild:true, clientOptions: {}, svgProxy: "https://svg.thei5pro.repl.co/svg.png?svg=$"}, options)
+  if(!token)throw new Error("Invalid/Missing token")
   let Discord = require("discord.js");
   Discord.Channel.prototype.svg = function(data){
-    return this.send(new Discord.MessageAttachment("https://svg.thei5pro.repl.co/svg.png?svg="+encodeURIComponent(data))) //SVG API
+    return this.send(new Discord.MessageAttachment(options.svgProxy.replace(/\$/g,encodeURIComponent(data)))) //SVG API
   }
-  let assetFor = a=>require("asset-js")(require("asset-js-firestore")(a,firetoken));
-  let user = assetFor("users");
-  let guild = assetFor("guilds");
-  let bot = new Discord.Client();
+  let blankAsset = a=>null
+  blankAsset.save = async a=>{}
+  let assetFor = firetoken ? a=>require("asset-js")(require("asset-js-firestore")(a,firetoken)) : a=>blankAsset;
+  let user = options.user ? assetFor("users") : blankAsset;
+  let member = options.member ? assetFor("members") : blankAsset;
+  let guild = options.guild ? assetFor("guilds") : blankAsset;
+  let bot = new Discord.Client(options.clientOptions);
   bot.Discord = Discord;
   Discord.Message.prototype.replyWith = Discord.Message.prototype.reply
   Discord.Message.prototype.reply = async function reply(content, options) {
     const mentionRepliedUser = typeof ((options || content || {}).allowedMentions || {}).repliedUser === "undefined" ? true : ((options || content).allowedMentions).repliedUser;
     delete ((options || content || {}).allowedMentions || {}).repliedUser;
-    const apiMessage = content instanceof APIMessage ? content.resolveData() : APIMessage.create(this.channel, content, options).resolveData();
+    const apiMessage = content instanceof Discord.APIMessage ? content.resolveData() : Discord.APIMessage.create(this.channel, content, options).resolveData();
     Object.assign(apiMessage.data, { message_reference: { message_id: this.id } });
     if (!apiMessage.data.allowed_mentions || Object.keys(apiMessage.data.allowed_mentions).length === 0) apiMessage.data.allowed_mentions = { parse: ["users", "roles", "everyone"] };
     if(typeof apiMessage.data.allowed_mentions.replied_user === "undefined") Object.assign(apiMessage.data.allowed_mentions, { replied_user: mentionRepliedUser });
@@ -25,16 +31,16 @@ module.exports=function(token,firetoken){
     const { data, files } = await apiMessage.resolveFiles();
     return this.client.api.channels[this.channel.id].messages.post({ data, files }).then(d => this.client.actions.MessageCreate.handle(d).message);
   }
-  bot.assets = {user, guild, save(){
+  firetoken && (bot.assets = {for: assetFor, user, guild, member, save(){
     return Promise.all([
       user.save(),
-      guild.save()
+      guild.save(),
+      member.save()
     ])
-  }}
-  let ai = new (require('alexa-bot-api'))();
-  ai = ai.getReply.bind(ai);
-  bot.ai = ai;
-  bot.assetFor = assetFor
+  }})
+  if(options.ai){
+    bot.ai = require('clever-bot-api');
+  }
   bot.login(token);
   function cmd(reg, hd){
     let res = []
@@ -67,7 +73,7 @@ module.exports=function(token,firetoken){
   }
   let pr = "!", rp = null, ph="Oops, an error occured. Here's the error: ```js\n$0\n```";
   bot.set = function(a){
-    pr = a.prefix || pr
+    pr = typeof a.prefix == "string" ? new RegExp("^"+a.prefix,"i") : a.prefix || pr
     rp = a.pingHint || rp
     ph = a.errorHint || ph
     return bot;
@@ -119,31 +125,32 @@ module.exports=function(token,firetoken){
     }
   })
   bot.on("message", async mg => {
-    if(mg.author.bot || mg.webhookID)return
-    if(mg.guild && !mg.guild.me.permissionsIn(mg.channel).has("SEND_MESSAGES"))return
-    let m = mg.content.slice(pr.length);
-    if(mg.content.match(new RegExp("^<@!?"+bot.id+">")))return mg.channel.send(rp||"Hey there, my prefix is `"+pr+"`").catch(e=>e)
+    if(mg.author.bot && !mg.webhookID)return
+    let m = mg.content.slice((mg.content.match(pr) || [""])[0].length);
+    if(mg.content.match(new RegExp("^<@!?"+bot.user.id+">")))return mg.channel.send(rp||"Hey there, my prefix is `"+pr.source+"`").catch(e=>e)
     let calculated = false;
-    let u, g, cx;
+    let u, g, mb, cx={};
     a: for(var i of cmd.stack){
-      if(!i[0].__prefixless && !mg.content.startsWith(pr))continue
+      if(!i[0].__prefixless && ((mg.guild && !mg.guild.me.permissionsIn(mg.channel).has("SEND_MESSAGES")) || m == mg.content || mg.webhookID))continue
       let pos = 0;
       let call = [mg]
       for(var q of i.slice(1)){
         let r = m.slice(pos).match(new RegExp("^(?:"+q[0].source+")(?= |$)",q[0].flags+"i")) || [""]
         if(r[0].length){
-          pos += r[0].length + 1;
-          r = await q[1](q[2]&1?r:r[0],mg)
-          q[0].source.match(/[^\w_-]/) && call.push(r)
+          pos += r[0].length;
+          pos += m.slice(pos).match(/^\s*/)[0].length
+          r = await q[1](q[2]&1?r:r[0],mg);
+          (q[0].source.match(/[^\w_-]/) || q[2]&2) && call.push(r)
         }else if(q[2]&2){
-          q[0].source.match(/[^\w_-]/) && call.push(undefined)
+          call.push(undefined)
         }else continue a
       }
       if(pos < m.length)continue
-      if(!calculated){
+      if(!calculated && firetoken){
         u = await user(mg.author.id)
         g = mg.guild ? await guild(mg.guild.id) : null
-        cx = {user:u,guild:g}
+        mb = mg.guild ? await member(mg.guild.id + "-" + mg.author.id) : null
+        cx = {user:u,guild:g,member:mb}
         calculated = true
       }
       try{if(!await i[0].apply(cx, call))break}catch(e){
@@ -153,6 +160,7 @@ module.exports=function(token,firetoken){
     }
   });
   bot.on("ready", () => {
+    if(bot.shard)return
     console.log("\x1b[1A\x1b[2KLogged in as \x1b[36m"+bot.user.username+"\x1b[33m#"+bot.user.discriminator)
   });
   return bot;
